f94209c62a262d0e55642b7b7cd44172b8f47130
Tabunov Egor <Vonubat@gmail.com>
2023-11-26 20:26:17 +0300

    Update base url. Update useUpsertAvailableProduct path

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,6 +1,6 @@
 const API_PATHS = {
   product:
-    "https://xhjhg6z360.execute-api.eu-west-1.amazonaws.com/prod/products",
+    "https://yl2b713fqc.execute-api.eu-west-1.amazonaws.com/prod/products",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
