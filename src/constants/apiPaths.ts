4dcd34de1c3755f8f552a384be12c8a83ee760d3
Tabunov Egor <Vonubat@gmail.com>
2023-12-03 18:01:18 +0300

    Update apiPaths.import url #2

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@ const API_PATHS = {
   product:
     "https://eq9tv9rnqg.execute-api.eu-west-1.amazonaws.com/prod/products",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://563yahw3yi.execute-api.eu-west-1.amazonaws.com/prod/import",
+  import: "https://6av9yyx5ig.execute-api.eu-west-1.amazonaws.com/prod/import",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
